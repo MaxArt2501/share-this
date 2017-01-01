5fd3aa23ed2ea0ed4fa4e12201a5f8d128df6f0f
Massimo Artizzu <maxart.x@gmail.com>
2017-01-01 14:40:16 +0100

    Changed module style to ES6; saved whitespaces on templates

--- src/sharers/facebook.js
+++ src/sharers/facebook.js
@@ -1,16 +1,25 @@
-exports.render = function render(text, rawText, refUrl) {
+export function render(text, rawText, refUrl) {
     const url = this.getShareUrl(text, refUrl);
 
-    return `<a href="${url}" target="_blank" rel="noopener nofollow noreferrer">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="1.5 1.5 16 16">
-            <path d="M8.546 16V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883
-                0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.48-1.195 1.18v1.54h2.39l-.31 2.42
-                h-2.08V16" fill="currentcolor"/>
-        </svg></a>`;
+    return `<a href="${url}" target="_blank" rel="noopener nofollow noreferrer">`
+        + "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"1.5 1.5 16 16\">"
+            + "<path d=\"M8.546 16V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883 "
+                + "0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.48-1.195 1.18v1.54h2.39l-.31 2.42"
+                + "h-2.08V16\" fill=\"currentcolor\"/>"
+        + "</svg></a>";
 };
 
-exports.getShareUrl = function getShareUrl(text, refUrl) {
+export function getShareUrl(text, refUrl) {
     return `https://www.facebook.com/sharer/sharer.php?quote=${encodeURIComponent(text)}&u=${encodeURIComponent(refUrl)}`;
 };
 
-exports.name = "facebook";
+export function action(event, item) {
+    event.preventDefault();
+    item.ownerDocument.defaultView.open(
+        item.firstChild.href,
+        "share_via_facebook",
+        "height=440,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,width=640"
+    );
+};
+
+export const name = "facebook";
--- src/sharers/linked-in.js
+++ src/sharers/linked-in.js
@@ -1,19 +1,19 @@
-exports.render = function render(text, rawText, refUrl) {
+export function render(text, rawText, refUrl) {
     const shareText = this.getText(text);
     const url = this.getShareUrl(shareText, refUrl);
 
-    return `<a href="${url}" target="_blank" rel="noopener nofollow noreferrer">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <path d="M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964
-                -1.424 1.96v3.778h-2.37V6H8.51v1.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0
-                2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758
-                .614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377z
-                m1.188 8.68H2.37V6h2.376v7.635z" fill="currentcolor"/>
-        </svg></a>`;
+    return `<a href="${url}" target="_blank" rel="noopener nofollow noreferrer">`
+        + "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\">"
+            + "<path d=\"M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964"
+                + "-1.424 1.96v3.778h-2.37V6H8.51v1.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0 "
+                + "2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758"
+                + ".614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377z"
+                + "m1.188 8.68H2.37V6h2.376v7.635z\" fill=\"currentcolor\"/>"
+        + "</svg></a>";
 };
 
 const CHAR_LIMIT = 250;
-exports.getText = function getText(text) {
+export function getText(text) {
     let chunk = text.trim();
     if (chunk.length > CHAR_LIMIT - 2)
         chunk = chunk.slice(0, CHAR_LIMIT - 3).trim() + "\u2026";
@@ -21,8 +21,17 @@ exports.getText = function getText(text) {
     return `\u201c${chunk}\u201d`;
 };
 
-exports.getShareUrl = function getShareUrl(text, refUrl) {
+export function getShareUrl(text, refUrl) {
     return `https://www.linkedin.com/shareArticle?mini=true&summary=${encodeURIComponent(text)}&url=${encodeURIComponent(refUrl)}`;
 };
 
-exports.name = "linked-in";
+export function action(event) {
+    event.preventDefault();
+    this.ownerDocument.defaultView.open(
+        this.firstChild.href,
+        "share_via_linked_in",
+        "height=440,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,width=640"
+    );
+};
+
+export const name = "linked-in";
